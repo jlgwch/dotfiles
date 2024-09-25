e9d9c9bbe299db7e354a317af71590d4ebb4c329
xuqiang <xuqiang@email.com>
2024-09-26 05:27:00 +0800

    update

--- config/ags/config.js
+++ config/ags/config.js
@@ -1,5 +1,6 @@
 import { Bar } from "./bar/bar.js"
 import { applauncher } from "./applauncher/applauncher.js"
+import * as Generic from './lib/utils.js'
 
 const { Gdk, Gtk } = imports.gi
 Gtk.IconTheme.get_default().append_search_path(`${App.configDir}/assets/icons`)
@@ -7,7 +8,7 @@ Gtk.IconTheme.get_default().append_search_path(`${App.configDir}/assets/icons`)
 App.config({
     style: "./style.css",
     windows: [
-        Bar(1),
+        Bar(Generic.getMonitorIDByName("eDP-1")),
         applauncher
 
         // you can call it, for each monitor
